7467b34ab98995f215cbe4190e76c28e2ef349ec
ianball99 <ianball99@gmail.com>
2026-03-09 13:31:21 +0000

    v7.1 after 3 new files from claude package,index,and.mainplus change to package

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import VamoosChat from "./VamoosChat";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <VamoosChat />
+  </React.StrictMode>
+);
